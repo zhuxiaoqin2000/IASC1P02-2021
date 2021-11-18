77e4f91bb54d874b6f00103c1cc6e8f24d92eb13
zhuxiaoqin2000 <hz18xq@brocku.ca>
2021-11-18 12:16:51 +0800

    Create scripts_ex3.js

---
+++ lab9/ex3/scripts_ex3.js
@@ -0,0 +1,8 @@
+
+function sum_of_two_numbers()
+{
+  var num1 = parseInt(document.getElementById("number1").value, 10);
+  var num2 = parseInt(document.getElementById("number2").value, 10);
+  var sum_1 = num1 + num2;
+  document.write(sum_1);
+}
